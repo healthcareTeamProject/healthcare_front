d0534baf647df57fbe09b32eb4546abaebb0b716
KIMDONGHYUN-rgb <s010314@naver.com>
2024-10-22 15:09:51 +0900

    kdh

--- src/Healthcare.tsx
+++ src/Healthcare.tsx
@@ -2,11 +2,13 @@ import React, { useEffect } from 'react';
 import './Healthcare.css';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import Main from 'src/views/Main';
+import Community from './views/Community';
 
 export default function Healthcare() {
   return (
     <Routes>
       <Route path='/main' element={<Main />} />
+      <Route path='/community' element={<Community />} />
     </Routes>
   );
 }
